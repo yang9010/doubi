4d975981a342edd488d2f1aa7534bb3505b06879
yang9010 <46948029+yang9010@users.noreply.github.com>
2020-12-17 11:31:07 +0800

    Update brook.sh
    try to test code

--- brook.sh
+++ brook.sh
@@ -102,9 +102,9 @@ Download_brook(){
 	[[ ! -e ${file} ]] && mkdir ${file}
 	cd ${file}
 	if [[ ${bit} == "x86_64" ]]; then
-		wget --no-check-certificate -N "https://github.com/txthinking/brook/releases/download/${brook_new_ver}/brook"
+		wget --no-check-certificate -N "https://github.com/txthinking/brook/releases/tag/${brook_new_ver}/brook"
 	else
-		wget --no-check-certificate -N "https://github.com/txthinking/brook/releases/download/${brook_new_ver}/brook_linux_386"
+		wget --no-check-certificate -N "https://github.com/txthinking/brook/releases/tag/${brook_new_ver}/brook_linux_386"
 		mv brook_linux_386 brook
 	fi
 	[[ ! -e "brook" ]] && echo -e "${Error} Brook 下载失败 !" && rm -rf "${file}" && exit 1
